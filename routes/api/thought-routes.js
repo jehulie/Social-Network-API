f7964a2c4259443533a08c86add1d79baedbf863
Julie Chu <julieychu@gmail.com>
2022-03-04 15:12:52 -0600

    update thought-routes code

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+
+const { 
+    getAllThoughts, 
+    getThoughtById, 
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+// GET all and POST at /api/thoughts
+router
+.route('/')
+.get(getAllThoughts)
+.post(createThought);
+
+// GET one, PUT, and DELETE at /api/thoughts/:id
+router
+.route('/:id')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
+
+// POST reaction
+router
+.route('/:thoughtId/reactions')
+.post(createReaction);
+
+// DELETE reaction
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+
+module.exports = router;
